4e92c3da3edf5c0c7e31feea5d583413f8afc25d
Nas <nassersanou23@gmail.com>
2019-02-17 16:55:04 -0500

    add Footer

---
+++ src/v1/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+
+    <footer id="section-footer" class="section blue darken-2 white-text center">
+    <p>Youlive Admin Pannel copyright &copy; 2019</p>
+    </footer>
+    
+  )
+}
